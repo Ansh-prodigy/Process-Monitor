3f186366194ba90ff11779fcdee6a41432349e8a
gautam <gautamrana4132@gmail.com>
2026-04-19 12:38:16 +0530

    Updated GUI and backend bridge

--- gui/main.py
+++ gui/main.py
@@ -3,3 +3,19 @@ from app import ProcessMonitorApp
 if __name__ == "__main__":
     app = ProcessMonitorApp()
     app.mainloop()
+from backend_bridge import get_processes
+
+def load_processes(tree):
+    for row in tree.get_children():
+        tree.delete(row)
+
+    data = get_processes()
+
+    for proc in data:
+        tree.insert("", "end", values=(
+            proc["pid"],
+            proc["name"],
+            proc["state"],
+            proc["cpu"],
+            proc["memory"]
+        ))
